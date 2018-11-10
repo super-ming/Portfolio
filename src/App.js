f865f6d49e339b1032a7a081c8666f41913d0b98
Ming Ho <hkboi108@hotmail.com>
2018-11-10 13:36:32 -0800

    add bootstrapped app with create-react-app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return (
+      <div className="App">
+        <nav className="App-header">
+
+
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
