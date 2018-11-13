da15157b0bd3bf2c6e0c22498015cab895ad13a3
Ming Ho <hkboi108@hotmail.com>
2018-11-13 14:18:44 -0800

    add restaurant project

--- src/Components/project_menu.js
+++ src/Components/project_menu.js
@@ -6,6 +6,7 @@ class Menu extends Component {
     super(props)
     this.state = {
       projects: [
+        {title: "Singapore Restaurants", src:"https://agile-beyond-13723.herokuapp.com/", text:"This application uses React to display restaurants in central Singapore. The locations are shown as markers on the Google Map and also as a list when the burger menu is activated. Information about the restaurant is gathered from Facebook Graph API and is displayed when either the marker or the list is clicked."},
         {title: "Cat Clicker", src:"https://super-ming.github.io/Cat-Clicker/", text:"For this project, I used HTML, CSS, and JavaScript to create an interactive application based on click events. When the user clicks on the cat photo, a sound clip is played and the click is recorded."},
         {title: "Feed Reader", src:"https://super-ming.github.io/frontend-nanodegree-feedreader/", text:"For this project, I used HTML, CSS, and JavaScript to create an application that provides a live feed of pages from Udacity."},
         {title: "Frogger Game", src:"https://super-ming.github.io/frontend-nanodegree-arcade-game/", text:"This is a classic arcade game in which the user controls the character at the bottom of the screen and tries to reach the river without touching an enemy bug. In the unfortunate event of touching a bug, the character goes back to his/her original position and have to start all over again. The calculations of the movement and timing was done with JavaScript"},
@@ -25,13 +26,13 @@ class Menu extends Component {
     const projects = this.state.projects;
     let activeKey = this.state.key;
     return(
-      <div>
-        <div className="projects">
+      <div className="project-wrapper">
+        <div id="projects" className="projects">
           <h3>Projects</h3>
         </div>
-        <div className="d-flex justify-content-center">
+        <div className="d-flex justify-content-center project-main">
           <Tabs
-            className="project-bar"
+            className="project-bar justify-content-center"
             activeKey={this.state.key}
             onSelect={this.handleSelect}
             variant="pills">
