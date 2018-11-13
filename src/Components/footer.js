995a5fbecfd47f594ff91398671304423565b0d1
Ming Ho <hkboi108@hotmail.com>
2018-11-12 18:43:26 -0800

    add image alt for accessibility

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { Nav, Navbar, NavItem, IconButton } from 'react-bootstrap'
+import { Nav, Navbar } from 'react-bootstrap'
 import { ReactComponent as Github } from '../Assets/Icons/github.svg'
 import { ReactComponent as LinkedIn } from '../Assets/Icons/linkedin.svg'
 
 const Footer = (props) => {
-  //<IconButton>
-    //<i className="material-icons">menu</i>
-  //</IconButton>
   return (
     <Navbar className="navbar justify-content-center" bg="light" variant="dark" expand="lg">
         <Nav className="social-icons" justify="true">
-          <Nav.Link href="https://github.com/super-ming"><Github /></Nav.Link>
-          <Nav.Link href="https://www.linkedin.com/in/mingyho"><LinkedIn fill="#0077B5"/></Nav.Link>
+          <Nav.Link href="https://github.com/super-ming"><Github alt="Github" /></Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/in/mingyho"><LinkedIn fill="#0077B5" alt="LinkedIn" /></Nav.Link>
         </Nav>
-
     </Navbar>
   )
 };
