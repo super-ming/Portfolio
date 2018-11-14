f468ab162105995f8a2e03f3ac53837589286bab
Ming Ho <hkboi108@hotmail.com>
2018-11-14 01:41:54 -0800

    add text animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './Styles/styles.css'
 import Navbar from './Components/navbar';
-import Footer from './Components/footer';
 import Menu from './Components/project_menu';
 import AboutMe from './Components/aboutme';
-import Photo from './Assets/Images/ming.jpg'
+import Photo from './Assets/Images/ming.jpg';
+import SplitText from 'react-pose-text';
 
+const charPoses = {
+exit: { opacity: 0, y: 20 },
+enter: {
+  opacity: 1,
+  y: 0,
+  delay: ({ charIndex }) => charIndex * 70
+}};
 
 class App extends Component {
 
@@ -16,10 +23,12 @@ class App extends Component {
           <Navbar className="navbar" />
         </nav>
         <section className="d-flex justify-content-center align-items-center intro">
-          <div>
-            <h2 className="mb-0">Ming Ho, Front End Web Developer</h2>
-          </div>
-          <span />
+          <h2 className="mb-0">
+            <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
+              Ming Ho, Front End Web Developer
+            </SplitText>
+          </h2>
+          <span className="underline"/>
         </section>
         <main className="main-section">
           <img src={Photo} alt="Ming Ho" className="profile-photo"/>
