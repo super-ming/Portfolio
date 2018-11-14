2953ca7aba93de71f34a605f9bfb8322c0075c06
Ming Ho <hkboi108@hotmail.com>
2018-11-14 01:50:15 -0800

    edit project text

--- src/Components/project_menu.js
+++ src/Components/project_menu.js
@@ -15,7 +15,7 @@ class Menu extends Component {
         {title: "Cat Clicker", href:"https://super-ming.github.io/Cat-Clicker/", src: CatClicker, text:"For this project, I used HTML, CSS, and JavaScript to create an interactive application based on click events. When the user clicks on the cat photo, a sound clip is played and the click is recorded."},
         {title: "Feed Reader", href:"https://super-ming.github.io/frontend-nanodegree-feedreader/", src: FeedReader, text:"For this project, I used HTML, CSS, and JavaScript to create an application that provides a live feed of pages from Udacity."},
         {title: "Frogger Game", href:"https://super-ming.github.io/frontend-nanodegree-arcade-game/", src: Frogger, text:"This is a classic arcade game in which the user controls the character at the bottom of the screen and tries to reach the river without touching an enemy bug. In the unfortunate event of touching a bug, the character goes back to his/her original position and have to start all over again. The calculations of the movement and timing was done with JavaScript"},
-        {title: "Memory Card Game", href:"https://super-ming.github.io/Memory-Card/", src: MemoryGame, text:"This is a classic memory game, in which the user clicks on a card to flip it over and the timer is activated. When the player flips another card and the icons on both cards are a match, the background color of the cards changes and the icon rotates. When there is no match, the cards closes and the thumbs up icon temporarily rotates to become a thumbs down icon. The player's score and rating is determined by the time spent and number of moves needed to complete the game. The player starts with three stars. One star is removed as the player spends more time and effort on the game."},
+        {title: "Memory Card Game", href:"https://super-ming.github.io/Memory-Card/", src: MemoryGame, text:"This is a classic memory game created with JavaScript, in which the user clicks on a card to flip it over and the timer is activated. When the player flips another card and the icons on both cards are a match, the background color of the cards changes and the icon rotates. When there is no match, the cards closes and the thumbs up icon temporarily rotates to become a thumbs down icon. The player's score and rating is determined by the time spent and number of moves needed to complete the game. The player starts with three stars. One star is removed as the player spends more time and effort on the game."},
         {title: "More...", src:"https://super-ming.github.io/Cat-Clicker/"}
       ],
       activeProject: [],
@@ -49,7 +49,7 @@ class Menu extends Component {
             ))}
           </Tabs>
         </div>
-        <div class="jumbo-wrapper">
+        <div className="jumbo-wrapper">
           <Jumbotron>
             <Media className="d-flex justify-content-between mediaGroup">
               <a href={projects[activeKey].href}>
@@ -64,6 +64,7 @@ class Menu extends Component {
             </Media>
           </Jumbotron>
           <a href="https://www.freepik.com/free-photos-vectors/background">Background vector created by Kjpargeter - Freepik.com</a>
+          <a href="https://icons8.com/">Envelope Icon provided by Icons8.com</a>
         </div>
       </div>
     )
