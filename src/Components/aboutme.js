a7b10874a5cdab590dc20328a286c467fbf80484
Ming Ho <hkboi108@hotmail.com>
2018-11-14 22:58:24 -0800

    move social media icons to footer and replace with tech icons

--- src/Components/aboutme.js
+++ src/Components/aboutme.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Media } from 'react-bootstrap';
-import { ReactComponent as Github } from '../Assets/Icons/github.svg'
-import { ReactComponent as LinkedIn } from '../Assets/Icons/linkedin.svg'
-import { ReactComponent as Envelope } from '../Assets/Icons/envelope.svg'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { ReactComponent as ReactIcon } from '../Assets/Icons/react.svg';
+import { ReactComponent as JavaScript } from '../Assets/Icons/javascript.svg';
+import { ReactComponent as Html5 } from '../Assets/Icons/html5.svg';
+import { ReactComponent as Css3 } from '../Assets/Icons/css3.svg';
+
+AOS.init();
 
 const aboutMe = (props) => {
   return (
-    <div id="about-me" className="about-me">
+    <div id="about-me" className="about-me" data-aos="fade-left" data-aos-offset="400" data-aos-duration="1000">
       <Media>
         <Media.Body className="about-me-body">
           <h3>About Me</h3>
           <p>Hi! I'm a web developer from Seattle. I began my programming journey in Jan 2018 and it has been quite a ride!
           I love learning about new things. You can also ask me anything travel related as I enjoy travelling and new experiences.
           Take a look at my projects below!</p>
-          <div className="social-icons">
-            <a href="https://github.com/super-ming"><Github alt="Github" /></a>
-            <a href="https://www.linkedin.com/in/mingyho"><LinkedIn alt="LinkedIn" /></a>
-            <a href="mailto:mingyho@gmail.com"><Envelope alt="Email" /></a>
+        <div className="tech-icons">
+            <ReactIcon alt="React Icon" className="react-icon"/>
+            <JavaScript alt="JavaScript Icon" className="js-icon"/>
+            <Html5 alt="HTML5 Icon" className="html-icon"/>
+            <Css3 alt="CSS3 Icon" className="css-icon"/>
           </div>
         </Media.Body>
       </Media>
--- src/Components/footer.js
+++ src/Components/footer.js
@@ -3,12 +3,20 @@ import { Nav, Navbar } from 'react-bootstrap'
 import { ReactComponent as Github } from '../Assets/Icons/github.svg'
 import { ReactComponent as LinkedIn } from '../Assets/Icons/linkedin.svg'
 
+//from https://github.com/danklammer/bytesize-icons
+const email = <svg alt="Email" id="i-mail" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+    <path d="M2 26 L30 26 30 6 2 6 Z M2 6 L16 16 30 6" /></svg>
+
 const Footer = (props) => {
   return (
-    <Navbar className="navbar justify-content-center" bg="light" variant="dark" expand="lg">
+    <Navbar className="justify-content-center" bg="light" variant="light" expand="lg">
+        <div className="attribution" >
+          <a href="https://www.freepik.com/free-photos-vectors/background">Background vector created by Kjpargeter - Freepik.com</a>
+        </div>
         <Nav className="social-icons" justify="true">
           <Nav.Link href="https://github.com/super-ming"><Github alt="Github" /></Nav.Link>
           <Nav.Link href="https://www.linkedin.com/in/mingyho"><LinkedIn fill="#0077B5" alt="LinkedIn" /></Nav.Link>
+          <Nav.Link href="mailto:mingyho@gmail.com">{email}</Nav.Link>
         </Nav>
     </Navbar>
   )
