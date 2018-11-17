d2133addc561e153dc2bddda39d489ad7925a302
Ming Ho <hkboi108@hotmail.com>
2018-11-16 23:24:40 -0800

    add click handling for arrow images

--- src/Components/project_menu.js
+++ src/Components/project_menu.js
@@ -6,18 +6,19 @@ import SingaporeMap from '../Assets/Images/singaporemap.JPG';
 import Frogger from '../Assets/Images/frogger.JPG';
 import MemoryGame from '../Assets/Images/memorygame.JPG';
 import Laptop from '../Assets/Images/laptop_frame.png';
+import { ReactComponent as ArrowRight } from '../Assets/Icons/arrowright.svg';
+import { ReactComponent as ArrowLeft } from '../Assets/Icons/arrowleft.svg';
 
 class Menu extends Component {
   constructor(props){
     super(props)
     this.state = {
       projects: [
-        {title: "Singapore Restaurants", href:"https://agile-beyond-13723.herokuapp.com/", src: SingaporeMap, tools: ['React', 'JavaScript', 'Html5', 'Css3', 'Facebook'], text:"This application uses React to display restaurants in central Singapore. The locations are shown as markers on the Google Map and also as a list when the burger menu is activated. Information about the restaurant is gathered from Facebook Graph API and is displayed when either the marker or the list is clicked."},
-        {title: "Cat Clicker", href:"https://super-ming.github.io/Cat-Clicker/", src: CatClicker, tools: ['JavaScript', 'Html5', 'Css3'], text:"For this project, I used HTML, CSS, and JavaScript to create an interactive application based on click events. When the user clicks on the cat photo, a sound clip is played and the click is recorded."},
-        {title: "Feed Reader", href:"https://super-ming.github.io/frontend-nanodegree-feedreader/", tools: ['JavaScript', 'Html5', 'Css3'], src: FeedReader, text:"For this project, I used HTML, CSS, and JavaScript to create an application that provides a live feed of pages from Udacity."},
-        {title: "Frogger Game", href:"https://super-ming.github.io/frontend-nanodegree-arcade-game/", tools: ['JavaScript', 'Html5', 'Css3'], src: Frogger, text:"This is a classic arcade game in which the user controls the character at the bottom of the screen and tries to reach the river without touching an enemy bug. In the unfortunate event of touching a bug, the character goes back to his/her original position and have to start all over again. The calculations of the movement and timing was done with JavaScript."},
-        {title: "Memory Card Game", href:"https://super-ming.github.io/Memory-Card/", src: MemoryGame, tools: ['JavaScript', 'Html5', 'Css3'], text:"This is a classic memory game created with JavaScript, in which the user clicks on a card to flip it over and the timer is activated. When the player flips another card and the icons on both cards are a match, the background color of the cards changes and the icon rotates. When there is no match, the cards closes and the thumbs up icon temporarily rotates to become a thumbs down icon. The player's score and rating is determined by the time spent and number of moves needed to complete the game. The player starts with three stars. One star is removed as the player spends more time and effort on the game."},
-        {title: "More...", src:"https://super-ming.github.io/Cat-Clicker/"}
+        {title: "Singapore Restaurants", href:"https://agile-beyond-13723.herokuapp.com/", src: SingaporeMap, tools: ['React', 'JavaScript', 'Html5', 'Css3', 'Facebook'], text:"This application uses React, Facebook Graph API, and Google Maps API to display restaurants in central Singapore."},
+        {title: "Cat Clicker", href:"https://super-ming.github.io/Cat-Clicker/", src: CatClicker, tools: ['JavaScript', 'Html5', 'Css3'], text:"Cat clicking application. Click if you dare."},
+        {title: "Feed Reader", href:"https://super-ming.github.io/frontend-nanodegree-feedreader/", tools: ['JavaScript', 'Html5', 'Css3'], src: FeedReader, text:"Application that provides a live feed of pages from various websites."},
+        {title: "Frogger Game", href:"https://super-ming.github.io/frontend-nanodegree-arcade-game/", tools: ['JavaScript', 'Html5', 'Css3'], src: Frogger, text:"This is a classic arcade game created with JavaScript, HTML, and CSS."},
+        {title: "Memory Card Game", href:"https://super-ming.github.io/Memory-Card/", src: MemoryGame, tools: ['JavaScript', 'Html5', 'Css3'], text:"This is a memory game created with JavaScript, HTML, and CSS."}
       ],
       activeProject: [],
       key: 0
@@ -37,11 +38,31 @@ class Menu extends Component {
     document.querySelector(".project-thumbnail").classList.add("active-project");
   }
 
+  forwardArrowSelect = () => {
+    let newKey = this.state.key;
+    if(this.state.key < 4){
+      newKey += 1;
+      this.handleSelect(newKey);
+    } else {
+      this.handleSelect(0);
+    }
+  }
+
+  backArrowSelect = () => {
+    let newKey = this.state.key;
+    if(this.state.key > 0){
+      newKey -= 1;
+      this.handleSelect(newKey);
+    } else {
+      this.handleSelect(4);
+    }
+  }
+
   render() {
     const projects = this.state.projects;
     let activeKey = this.state.key;
     return(
-      <div className="project-wrapper" data-aos="fade-up" data-aos-offset="400" data-aos-duration="1000">
+      <div className="project-wrapper" data-aos="fade-up" data-aos-offset={window.innerWidth < 600 ? 400 : (200)} data-aos-duration="1000">
         <div id="projects" className="projects">
           <h3>Projects</h3>
         </div>
@@ -61,13 +82,17 @@ class Menu extends Component {
         </div>
         <div className="jumbo-wrapper">
           <Jumbotron>
-            <Media className="d-flex justify-content-between mediaGroup">
+            <Media className="justify-content-between mediaGroup">
               <a href={projects[activeKey].href}>
                 <img alt="laptop frame" className="laptop" src={Laptop}></img>
                 <img className="project-thumbnail"
                 alt={projects[activeKey].title}
                 src={projects[activeKey].src} />
               </a>
+              <div className="d-flex justify-content-center nav-arrow">
+                <ArrowLeft className="arrow-left" width="34" onClick={this.backArrowSelect}/>
+                <ArrowRight className="arrow-right" width="34" onClick={this.forwardArrowSelect}/>
+              </div>
               <Media.Body className="project-text">
                 <h4>{projects[activeKey].title}</h4>
                 <p>{projects[activeKey].text}</p>
