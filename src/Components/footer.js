e1d829f3493b76b4a3f3e1340093d7922af94439
Ming Ho <hkboi108@hotmail.com>
2018-11-18 22:46:08 -0800

    update class names

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Row} from 'react-bootstrap'
+import { Nav, Navbar, Row } from 'react-bootstrap'
 import { ReactComponent as Github } from '../Assets/Icons/github.svg'
 import { ReactComponent as LinkedIn } from '../Assets/Icons/linkedin.svg'
 
@@ -9,14 +9,16 @@ const email = <svg alt="Email" id="i-mail" viewBox="0 0 32 32" width="32" height
 
 const Footer = (props) => {
   return (
-        <Nav className="social-icons justify-content-centers " >
-          <Row className="d-flex justify-content-center row">
-            <Nav.Link href="https://github.com/super-ming"><Github alt="Github" /></Nav.Link>
-            <Nav.Link href="https://www.linkedin.com/in/mingyho"><LinkedIn fill="#0077B5" alt="LinkedIn" /></Nav.Link>
-            <Nav.Link href="mailto:mingyho@gmail.com">{email}</Nav.Link>
-            <Nav.Link href="https://www.freepik.com/free-photos-vectors/background">Background vector created by Kjpargeter - Freepik.com</Nav.Link>
-          </Row>
-        </Nav>
+    <Navbar className="d-flex justify-content-center align-items-center">
+      <Nav className="social-icons" >
+        <Row className="d-flex justify-content-center row">
+          <Nav.Link href="https://github.com/super-ming"><Github alt="Github" /></Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/in/mingyho"><LinkedIn fill="#0077B5" alt="LinkedIn" /></Nav.Link>
+          <Nav.Link href="mailto:mingyho@gmail.com">{email}</Nav.Link>
+          <Nav.Link href="https://www.freepik.com/free-photos-vectors/background">Background vector created by Kjpargeter - Freepik.com</Nav.Link>
+        </Row>
+      </Nav>
+    </Navbar>
   )
 };
 
